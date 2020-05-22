2ee204a09d97ede0de31ffd3672fc14622ec9d2c
Natalia Stewart <60708462+nataliastewart@users.noreply.github.com>
2020-05-21 20:41:15 -0400

    initial commit

---
+++ app/src/reducers/pageReducer.js
@@ -0,0 +1,33 @@
+const initialState = {
+  Countries: [{ Country: "", TotalConfirmed: "", TotalDeaths: "" }],
+  isFetching: false,
+  error: "",
+};
+
+export const pageReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "FETCH_DATA_START":
+      return {
+        ...state,
+        isFetching: true,
+      };
+    case "FETCH_DATA_SUCCESS":
+      return {
+        ...state,
+        Countries: action.payload,
+
+        isFetching: false,
+        error: "",
+      };
+    default:
+      return state;
+  }
+};
+// Countries: [
+//   ...state.Countries,
+//   {
+//     Country: action.payload.Country,
+//     TotalConfirmed: action.payload.TotalConfirmed,
+//     TotalDeaths: action.payload.TotalDeaths,
+//   },
+// ],
